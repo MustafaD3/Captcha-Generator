f536dd3ecac732b8eb423672d73c7486982512e5
Mustafa Dalkılıç <81879117+MustafaD3@users.noreply.github.com>
2023-06-07 20:25:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express"
 const app = express()
-const port = 4000 || 5000
+const port = 5000
 app.listen(port,()=>{
     console.log("Sunucu "+port+" Portundan Dinleniyor")
     app.use("/public",express.static("./public"))
